7dfae768540736682b5be049c4ea65c8c851e081
SwiftNicholas <17603640+SwiftNicholas@users.noreply.github.com>
2023-02-12 19:26:46 -0500

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-IP_ADDRESS=$(ip addr show | grep -Eo 'inet (addr:)?([0-9].){3}[0-9]' | grep -Eo '([0-9].){3}[0-9]' | grep -v '127.0.0.1')
+IP_ADDRESS=$(ip addr show eth0 | grep -Eo 'inet (addr:)?([0-9].){3}[0-9]' | grep -Eo '([0-9].){3}[0-9]' | grep -v '127.0.0.1')
 echo "MYSQL_PASSWORD=$MYSQL_PASSWORD"
 echo "IP_ADDRESS=$IP_ADDRESS"
 
